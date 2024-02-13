74044458e667fa73f7c37775176b7dc3760b8af7
Max Kopinsky <zerglingk9012@gmail.com>
2024-02-13 03:38:31 -0500

    Free up some registers for ECHO

--- wozmon/wozmon.s
+++ wozmon/wozmon.s
@@ -4,27 +4,29 @@
 
                 .ifdef VARS_IN_BSS
                 .bss
-VARS:           .space 46
+VARS:           .space 136
                 .endif
 
                 .ifndef VARS
                 .err
                 .endif
 
-                .set DATA, VARS+6 ; ptr to IN, other vars are at negative offsets
+                .set DATA, VARS+8 ; ptr to IN, other vars are at negative offsets
 
                 ; most of these can probably actually be in registers
                 ; need to write more code and see how they are used.
                 ; Register allocation:
                 ; r0-r1 t
-                ; r2-r3 t. r2 sometimes holds hex data. r3 sometimes saves r4.
-                ; r4    text index (Y). t when text index not needed
-                ; r5    one-level subroutine nesting return address
+                ; r2-r4 t. r2 sometimes holds hex data. r3 sometimes saves r5.
+                ; r5    text index (Y). t when text index not needed
                 ; r6    value of DATA
                 ; r7    return address
                 ; When r6 is not a nested return address, it is t
-                .set XAM_OFF,  -6 ; 2 bytes
-                .set ST_OFF,   -4 ; 2 bytes
+                .set XAM_OFF,  -8 ; 2 bytes
+                .set ST_OFF,   -6 ; 2 bytes
+                ; There are 3 blank words of space here, which are used in
+                ; PRBYTE to save some temporary data to free up registers
+                ; for custom ECHO functions.
                 .set MODE_OFF, -1 ; 1 byte
 
                 ; defn of _start is needed for MegaIng's annotated output
@@ -32,7 +34,7 @@ VARS:           .space 46
                 .globl _start
 _start:
                 .globl WOZ_RESET
-WOZ_RESET:      movh    %rh4, 0x7F
+WOZ_RESET:      movh    %rh5, 0x7F
                 call    1f
                 .word   DATA
 1:              movx    %rx6, [%rx7]
@@ -47,14 +49,14 @@ NOTCR:          movh    %rh1, '_'       ; '_' -> r0
                 je      BACKSPACE
                 subh    %rh0, 1         ; cmp char =? '`' ('`' = '_' + 1)
                 je      ESCAPE
-                addh    %rh4, 1         ; INY advance text index
+                addh    %rh5, 1         ; INY advance text index
                 jnn     NEXTCHAR        ; auto ESC if > 127
 ESCAPE:         movh    %rh0, '\\'      ; '\' -> r0
-                storeh  %rh0, DSP       ; output it
+                call WOZ_ECHO       ; output it
 GETLINE:        movh    %rh0, 10        ; LF -> r0
-                storeh  %rh0, DSP       ; Output it
-                movh    %rh4, 1         ; initialize text index
-BACKSPACE:      subh    %rh4, 1         ; Back up text index
+                call WOZ_ECHO       ; Output it
+                movh    %rh5, 1         ; initialize text index
+BACKSPACE:      subh    %rh5, 1         ; Back up text index
                 jn      GETLINE         ; Beyond start of line, reinit
 NEXTCHAR:       movx    %rx1, 0x100     ; 0x0100 -> r1
 1:              loadh   %rh0, KBDCR     ; Key ready?
@@ -68,20 +70,20 @@ NEXTCHAR:       movx    %rx1, 0x100     ; 0x0100 -> r1
                 movh    %rh1, 0x15      ; TC keycode for shift
                 cmph    %rh0, %rh1      ; key = SHIFT?
                 je      NEXTCHAR        ; loop if yes.
-                movh    %rh1, %rh4      ; Y -> r1
+                movh    %rh1, %rh5      ; Y -> r1
                 addx    %rx1, %rx6      ; IN+Y -> r1
                 movh    [%rx1], %rh0    ; add char to text buffer
                 call    WOZ_ECHO_0      ; Output character
                 cmph    %rh0, 10        ; LF? (TC Keyboards send 5, but ECHO_0 fixed)
                 jne     NOTCR           ; no
-                movh    %rh4, -1        ; Reset text index
+                movh    %rh5, -1        ; Reset text index
                 mov     %rh0, 0xD       ; D -> r0 For XAM mode
                 ; 0 = BLOCK XAM, 0xD = XAM, 0xC = STOR (need XAM > STOR)
 SETMODE:        movx    %rx1, MODE_OFF  ; MODE -> r1
                 addx    %rx1, %rx6      ; DATA+MODE -> r1
                 movh    [%rx1], %rh0    ; r0 -> (DATA,MODE)
-BLSKIP:         addh    %rh4, 1         ; advance text index
-NEXTITEM:       movh    %rh0, %rh4      ; Y -> r0
+BLSKIP:         addh    %rh5, 1         ; advance text index
+NEXTITEM:       movh    %rh0, %rh5      ; Y -> r0
                 addx    %rx0, %rx6      ; DATA+Y -> r0
                 movh    %rh0, [%rx0]    ; (IN,Y) -> r0
                 cmph    %rh0, 5         ; LF?
@@ -96,8 +98,8 @@ NEXTITEM:       movh    %rh0, %rh4      ; Y -> r0
                 cmph    %rh0, %rh1      ; char - '.' =? 'R' - '.'
                 je      RUN             ; yes, run user program
                 movx    %rx2, 0         ; 0 -> r2 [L,H]
-                movh    %rh3, %rh4      ; Y -> r3 to check if changed later
-NEXTHEX:        movx    %rx0, %rx4      ; Y -> r0
+                movh    %rh3, %rh5      ; Y -> r3 to check if changed later
+NEXTHEX:        movx    %rx0, %rx5      ; Y -> r0
                 addx    %rx0, %rx6      ; IN+Y -> r0
                 movh    %rh0, [%rx0]    ; (IN,Y) -> r0
                 movh    %rh1, '0'       ; '0' -> r1, note '0' is 0x30
@@ -114,9 +116,9 @@ DIGIT:          andh    %rh0, 0x0F      ; least significant nibble is hex digit
                 addx    %rx2, %rx2
                 addx    %rx2, %rx2      ; r2<<4 -> r2
                 orx     %rx2, %rx0      ; r2|r0 -> r2
-                addh    %rh4, 1         ; INY advance text index
+                addh    %rh5, 1         ; INY advance text index
                 jmp     NEXTHEX         ; Check next char for hex
-NOTHEX:         cmph    %rh4, %rh3      ; Check if we read any digits
+NOTHEX:         cmph    %rh5, %rh3      ; Check if we read any digits
                 je      ESCAPE          ; No, generate ESC sequence.
                 movx    %rx1, MODE_OFF  ; MODE -> r1
                 addx    %rx1, %rx6      ; DATA+MODE -> r1
@@ -141,7 +143,7 @@ NOTSTOR:        jb      XAMNEXT         ; from cmp (MODE),0xC less => block XAM
                 xorx    %rx3, %rx3      ; set Z flag
 NXTPRNT:        jnz     PRDATA          ; NZ means no address to print
                 movh    %rh0, 10        ; LF -> r0
-                storeh  %rh0, DSP       ; Output it
+                call WOZ_ECHO       ; Output it
                 addx    %rx1, 1         ; DATA+XAM{H} -> r1
                 movh    %rh0, [%rx1]    ; (DATA,XAMH) -> r0
                 call    PRBYTE          ; Output it in hex format
@@ -149,9 +151,9 @@ NXTPRNT:        jnz     PRDATA          ; NZ means no address to print
                 movh    %rh0, [%rx1]    ; (DATA,XAM) -> r0
                 call    PRBYTE          ; Output it in hex format
                 movh    %rh0, ':'       ; ':' -> r0
-                storeh  %rh0, DSP       ; Output it
+                call WOZ_ECHO       ; Output it
 PRDATA:         movh    %rh0, ' '       ; ' ' -> r0
-                storeh  %rh0, DSP       ; Output it
+                call WOZ_ECHO       ; Output it
                 movx    %rx0, [%rx1]    ; (DATA,XAM) -> r0
                 movh    %rh0, [%rx0]    ; ((DATA,XAM)) -> r0
                 call    PRBYTE          ; Output it in hex format
@@ -165,12 +167,11 @@ XAMNEXT:        pushh   0xD             ; Reset MODE to XAM mode
 MOD8CHK:        andx    %rx0, 0x07      ; (XAM) mod 8
                 jmp     NXTPRNT
 PRBYTE:         movh    %rh3, %rh0      ; r0 -> r3 to save around PRHEX
-                movx    %rx5, %rx7      ; r7 -> r5 to save ret addr
                 ; This code MASSIVELY benefits from conditional use of EXOP.
                 ; Right-shift %rh0 by 4, by left shifting %rx0 by 4 and then
                 ; extracting the upper byte through memory.
                 ; The memory that we use is (MODE), which is currently
-                ; irrelevant since it will be reset by NEWITEM.
+                ; irrelevant since it will be reset before NEXTITEM.
                 addx    %rx0, %rx0
                 addx    %rx0, %rx0
                 addx    %rx0, %rx0
@@ -178,22 +179,23 @@ PRBYTE:         movh    %rh3, %rh0      ; r0 -> r3 to save around PRHEX
                 pushx   %rx0            ; r0<<4 -> (MODE)
                 addx    %rx6, 1
                 poph    %rh0            ; r0>>4 -> r0
+                pushx   %rx7            ; save ret addr around PRHEX
+                pushh   %rh3            ; save rh3 around PREHX
                 call    PRHEX           ; Output hex digit
-                movx    %rx7, %rx5      ; restore return address
-                movh    %rh0, %rh3      ; restore LSD
+                poph    %rh0            ; restore LSD (saved from rh3)
+                popx    %rx7            ; restore ret addr
 PRHEX:          andh    %rh0, 0x0F      ; mask LSD
                 cmph    %rh0, 10        ; digit?
                 jb      1f              ; yes, don't add letter offset
                 addh    %rh0, 7         ; 'A' - '0' + 0xA
-1:              ; This code would benefit from FI or really just anyway way
+1:              ; This code would benefit from FI or really just any way
                 ; of easily adding '0' without needing a register.
-                addh    %rh0, 0xC
-                addh    %rh0, 0xC
-                addh    %rh0, 0xC       ; r0 + '0' -> r0
-                addh    %rh0, 0xC
+                movh    %rh3, '0'
+                addh    %rh0, %rh3      ; r0 + '0' -> r0
 WOZ_ECHO_0:     cmph    %rh0, 5         ; TCLF?
                 jne     WOZ_ECHO        ; no, just echo it
                 addh    %rh0, 5         ; TCLF => LF
+                ; free registers here: r0,r3,r4
                 .globl WOZ_ECHO
 WOZ_ECHO:       storeh  %rh0, DSP       ; output character
                 ret
